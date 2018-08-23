470c344c34bad7811fafa5ca07fd6a688528cf53
SamuelMuloki <samuellebron0@gmail.com>
2018-08-23 18:24:36 +0300

    Fix adds script multiple times

--- src/components/GoogleApi.ts
+++ src/components/GoogleApi.ts
@@ -6,7 +6,7 @@ export interface GoogleApiWrapperState {
     alertMessage?: string;
 }
 
-const googleApiWrapper = (script?: string) => <P extends GoogleMapsProps>(wrappedComponent: ComponentType<P>) => {
+const googleApiWrapper = (script: string) => <P extends GoogleMapsProps>(wrappedComponent: ComponentType<P>) => {
     class GoogleApiWrapperComponent extends Component<P, GoogleApiWrapperState> {
         readonly state: GoogleApiWrapperState = { scriptsLoaded: false, alertMessage: "" };
 
@@ -20,13 +20,11 @@ const googleApiWrapper = (script?: string) => <P extends GoogleMapsProps>(wrappe
         }
 
         componentDidMount() {
-            if (!this.state.scriptsLoaded) {
-                this.loadScript(script);
-            }
+            this.loadScript(script);
         }
 
-        private loadScript = (googleScript?: string) => {
-            if (googleScript) {
+        private loadScript = (googleScript: string) => {
+            if (!document.querySelectorAll(`[src="${googleScript + this.props.googleMapsToken}"]`).length) {
                 document.body.appendChild(Object.assign(
                     document.createElement("script"), {
                         type: "text/javascript",
@@ -34,6 +32,8 @@ const googleApiWrapper = (script?: string) => <P extends GoogleMapsProps>(wrappe
                         onload: () => this.setState({ scriptsLoaded: true }),
                         onerror: () => this.setState({ alertMessage: "Could not load. Please check your internet connection" })
                     }));
+            } else {
+                this.setState({ scriptsLoaded : true });
             }
         }
 
